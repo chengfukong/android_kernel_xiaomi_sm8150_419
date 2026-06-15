f7d897e9c436163786f683dbb0ac212c3f2ec9e1
duckyduckG <duckyduckg65@gmail.com>
2026-06-15 06:00:46 +0500

    Revert "WIP: msm/vidc: Update msmnile support"
    This reverts commit c1c8531b1f3f9fb44328936da75e65169126ecc0.

--- techpack/video/msm/vidc/vidc_hfi_helper.h
+++ techpack/video/msm/vidc/vidc_hfi_helper.h
@@ -601,13 +601,12 @@ struct hfi_profile_level {
 };
 
 #ifdef CONFIG_MSMNILE_SUPPORT
-struct hfi_dpb_counts_msmnile {
+struct hfi_dpb_counts {
 	u32 max_dpb_count;
 	u32 max_ref_frames;
 	u32 max_dec_buffering;
 };
-#endif
-
+#else
 struct hfi_dpb_counts {
 	u32 max_dpb_count;
 	u32 max_ref_frames;
@@ -615,6 +614,7 @@ struct hfi_dpb_counts {
 	u32 max_reorder_frames;
 	u32 fw_min_cnt;
 };
+#endif
 
 struct hfi_profile_level_supported {
 	u32 profile_count;
